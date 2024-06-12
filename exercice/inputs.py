284c77eb124d3d2ee286d0fbecb7c576cab64937
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-12 04:26:46 -0500

    feat: Add code to calculate the average of three numbers

---
+++ exercice/inputs.py
@@ -0,0 +1,8 @@
+a = float(input("Ingrese el primer valor: "))
+b = float(input("Ingrese el segundo valor: "))
+c = float(input("Ingrese el tercer valor: "))
+
+r = (a + b + c) / 3
+rStr = f'{a} + {b} + {c} / 3 ='
+
+print(f"El resultado de la operación {rStr} es igual a {r}")
